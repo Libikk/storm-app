9999882e6bce0a53f17d99e0dd1128d4061a24fb
Libikk <libikpl@gmail.com>
2019-08-01 20:15:41 +0100

    vutify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,56 @@
+//import colors from "vuetify/es5/util/colors";
+
 export default {
-    
-}
+  /*
+   ** Rendering mode
+   ** Doc: https://nuxtjs.org/api/configuration-mode
+   */
+  mode: "universal",
+
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js Vuetify starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js Vuetify starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/api/configuration-css
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://nuxtjs.org/guide/modules
+   */
+  modules: [
+    "@nuxtjs/vuetify",
+    // TODO: Remove it if you want to eject from codeSandbox
+  ],
+
+  // Doc: https://github.com/nuxt-community/vuetify-module
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: true
+    }
+  }
+};
