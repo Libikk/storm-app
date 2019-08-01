b8636bda798ba1f6c0dbf9f2131b82cab636b152
Libikk <libikpl@gmail.com>
2019-08-01 20:16:35 +0100

    basic footer and header

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+ <template>
+    <v-footer>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar class="app-bar">
+        <div>
+            <img src="~/assets/StormLogo.png" class="app-bar__logo">
+        </div>
+        <div class="app-bar__menu">
+            <v-btn class="global-button__primary">Login</v-btn>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </div>
+    </v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+.app-bar {
+    max-height: 120px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &__logo {
+        width: 115px;
+    }
+
+    &__menu {
+
+    }
+
+}
+</style>
