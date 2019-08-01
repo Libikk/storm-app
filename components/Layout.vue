9fa9c76460f1670aa9cafbc94d3c0f8894651120
Libikk <libikpl@gmail.com>
2019-08-01 20:16:58 +0100

    layout component to wrap app

---
+++ components/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app id="inspire">
+    <Header />
+        <v-content>
+            <slot/>
+        </v-content>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from './Footer';
+import Header from './Header'
+
+export default {
+    components: {
+        Footer,
+        Header
+    },
+  methods: {
+      test() {
+          console.log(this, this.$store)
+      }
+  },
+  data: () => ({
+      drawer: null,
+    }),
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/variables.scss';
+
+.global-button__primary {
+    color: $white;
+
+    :hover {
+        color: $red;
+    }
+}
+
+</style>
