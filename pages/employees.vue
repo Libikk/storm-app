7e03fbd99e52afdbb6408cbaf836b901dcb8d827
Libikk <libikpl@gmail.com>
2019-08-03 19:41:44 +0100

    initial design employee table

--- pages/employees.vue
+++ pages/employees.vue
@@ -1,19 +1,69 @@
 <template>
   <Layout>
-    <h1>Welcome on secret page.</h1>
-    <NuxtLink to="/">
-      Back home
-    </NuxtLink>
+      <div>
+         <v-data-table
+            :headers="headers"
+            :items="employeeList"
+            :items-per-page="5"
+            :search="search"
+          >
+          <template v-slot:item.employeeDepartments="{ item }">
+            <ul>
+              <li v-for="sinlgDep in item.employeeDepartments" :key="sinlgDep.departmentId">
+                  {{ sinlgDep.employeeDepartment.description }}
+              </li>
+            </ul>
+          </template>
+          </v-data-table>
+      </div>
   </Layout>
 </template>
 
 <script>
 
 import Layout from '../components/Layout';
+import EmployeesService from '../api/employeesService';
+
 export default {
   middleware: 'authenticated',
   components: {
     Layout
-  }
+  },
+  asyncData ({ req, res, store }) {
+    if (!store.state.employees.length) {
+        EmployeesService.getListOfEmployees()
+          .then(res => store.commit('setEmployees', res.results))
+    }
+  },
+  methods: {
+    renderDepartments(departments) {
+      return departments.map(e => e.employeeDepartment.description)
+    }
+  },
+  data: () => ({
+    search: '',
+    headers: [
+      {
+        text: 'Id',
+        align: 'left',
+        sortable: false,
+        value: 'id',
+      },
+      { text: 'Name', value: 'name' },
+      { text: 'Email', value: 'email' },
+      { text: 'Created at', value: 'createdDate' },
+      { text: 'Departments', value: 'employeeDepartments' },
+      { text: 'Status', value: 'isDeleted' },
+    ],
+  }),
+  computed: {
+    employeeList() {
+      return this.$store.state.employees.map((singleEmployee) => ({
+        ...singleEmployee,
+        name: `${singleEmployee.first_Name} ${singleEmployee.last_Name}`,
+        isDeleted: singleEmployee.isDeleted ? 'Active' : 'Deleted',
+      }))
+    }
+  },
 }
 </script>
