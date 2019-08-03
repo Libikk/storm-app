cc4f8480e855ab8a7d8f51721fa19146ca686217
Libikk <libikpl@gmail.com>
2019-08-03 13:25:53 +0100

    check if user exist in db and return that

---
+++ assets/userCollection.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        id: '225',
+        userLogin: 'StormTest',
+        name: "Enthusiastic Candidate",
+        password: "-!NmmQR2pbmJSQ7", // this would be encryptet
+    }
+]
--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+import userCollection from '../assets/userCollection';
 
 export const getCookie = (name, cookie) => {
     const escape = (s) => s.replace(/([.*+?\^${}()|\[\]\/\\])/g, '\\$1');
@@ -6,10 +7,17 @@ export const getCookie = (name, cookie) => {
     return match ? match[1] : null;
 }
 
-export const getCookieData = (requestToken) => {
-    const isValid = true // verify cookie with secret key and check if not expired, jwt.verify(...)
-    if (isValid) {
-        return jwt.decode(requestToken, { complete: true }).payload
+export const authUser = (requestToken) => {
+    // verify cookie with secret key and check if it's not expired, jwt.verify(...)
+    const isValid = true
+    //
+
+    if (isValid && requestToken) {
+        var { primarysid, unique_name } = jwt.decode(requestToken, { complete: true }).payload
+        var user = userCollection.find(({ id, name }) => id === primarysid && name === unique_name )
+        if (user) {
+            return { name: user.name, id: user.id };
+        }
     }
-    return {};
+    return null;
 }
