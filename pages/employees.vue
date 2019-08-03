22c5f5bb00b7977781d3afc0b7c7fcf4d93bc0dd
Libikk <libikpl@gmail.com>
2019-08-03 20:35:57 +0100

    apply dynamic filters for each field

--- pages/employees.vue
+++ pages/employees.vue
@@ -1,20 +1,33 @@
 <template>
   <Layout>
       <div>
-         <v-data-table
-            :headers="headers"
-            :items="employeeList"
-            :items-per-page="5"
-            :search="search"
-          >
-          <template v-slot:item.employeeDepartments="{ item }">
-            <ul>
-              <li v-for="sinlgDep in item.employeeDepartments" :key="sinlgDep.departmentId">
-                  {{ sinlgDep.employeeDepartment.description }}
-              </li>
-            </ul>
-          </template>
+        <v-card>
+          <v-card-title>
+            Employees
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              label="Search..."
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+              :headers="headers"
+              :items="employeeList"
+              :items-per-page="5"
+              :loading="isLoading"
+              loading-text="Loading... Please wait"
+            >
+            <template v-slot:item.employeeDepartments="{ item }">
+              <ul>
+                <li v-for="sinlgDep in item.employeeDepartments" :key="sinlgDep.departmentId">
+                    {{ sinlgDep.employeeDepartment.description }}
+                </li>
+              </ul>
+            </template>
           </v-data-table>
+          </v-card>
       </div>
   </Layout>
 </template>
@@ -38,17 +51,23 @@ export default {
   methods: {
     renderDepartments(departments) {
       return departments.map(e => e.employeeDepartment.description)
-    }
+    },
+    getFieldStringValue(propertyName, propertyValue) {
+      var convertedValue
+      if (propertyName === 'isDeleted') {
+        convertedValue = propertyValue ? 'Active' : 'Deleted'
+      }
+      if (propertyName === 'employeeDepartments') {
+        convertedValue = propertyValue.reduce((acc, nextDep) => `${acc} ${nextDep.employeeDepartment.description}`, '')
+      }
+      return String(convertedValue || propertyValue)
+    },
   },
   data: () => ({
     search: '',
+    isLoading: false,
     headers: [
-      {
-        text: 'Id',
-        align: 'left',
-        sortable: false,
-        value: 'id',
-      },
+      { text: 'Identifier',  value: 'identifier' },
       { text: 'Name', value: 'name' },
       { text: 'Email', value: 'email' },
       { text: 'Created at', value: 'createdDate' },
@@ -57,11 +76,23 @@ export default {
     ],
   }),
   computed: {
+    filteredEmployees() {
+      return this.$store.state.employees.filter(singleEmp => {
+        if (this.search) {
+            return this.headers.reduce((acc, nextCol) => {
+              const propertyName = nextCol.value
+              const fieldValue = this.getFieldStringValue(propertyName, singleEmp[nextCol.value]).toLowerCase()
+              return acc || fieldValue.includes(this.search);
+            }, false)
+        }
+        return true
+      })
+    },
     employeeList() {
-      return this.$store.state.employees.map((singleEmployee) => ({
+      return this.filteredEmployees.map((singleEmployee) => ({
         ...singleEmployee,
         name: `${singleEmployee.first_Name} ${singleEmployee.last_Name}`,
-        isDeleted: singleEmployee.isDeleted ? 'Active' : 'Deleted',
+        isDeleted: this.getFieldStringValue('isDeleted', singleEmployee.isDeleted)
       }))
     }
   },
