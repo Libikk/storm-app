ea1b7c446ada90f509462809a5a3fbc828266583
Libikk <libikpl@gmail.com>
2019-08-04 17:21:32 +0100

    editable fields and simple validation

--- api/employeesService.js
+++ api/employeesService.js
@@ -4,4 +4,7 @@ export default class EmployeesService {
     static getListOfEmployees = async () => (this.$axios || axios).get('/api/employee?pageSize=1000&Include=EmployeeDepartments&Include=EmployeeDepartment').then(({ data }) => data);
 
     static getEmployeeDepartments = async () => (this.$axios || axios).get(`/api/department/?pageSize=100&isDeleted=0`).then(({ data }) => data);
+
+    static updateEmployeeDetails = async (personDetails) => (this.$axios || axios).post(`/api/employee/`, personDetails).then(({ data }) => data);
+
 }
--- components/employees/editableEmployee.vue
+++ components/employees/editableEmployee.vue
@@ -1,6 +1,63 @@
 <template>
-      <div>
-          EDITABLE EMPLOYEE {{ employeeId }}
+      <div class="edit-employee-container">
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+
+            <v-text-field
+                v-model="personData.identifier"
+                label="Identifier"
+                disabled
+            />
+
+            <v-text-field
+                v-model="personData.first_Name"
+                :counter="15"
+                label="First Name"
+                required
+                :rules="nameRules"
+            />
+
+            <v-text-field
+                v-model="personData.last_Name"
+                :counter="15"
+                label="Last Name"
+                :rules="nameRules"
+                required
+            />
+
+            <v-text-field
+                v-model="personData.email"
+                label="Email"
+                :rules="emailRules"
+                required
+            />
+
+            <v-select
+                v-model="personData.departments"
+                :items="$store.state.departments"
+                label="Departments"
+                 item-text="description"
+                multiple
+            />
+            <v-select
+                v-model="personData.isDeleted"
+                :rules="[v => !!v || 'Item is required']"
+                label="isDeleted"
+                :items="['Active', 'Deleted']"
+                required
+            />
+
+            <v-btn
+                :disabled="!valid"
+                color="success"
+                @click="updatePersonDetails"
+            >
+                Update
+            </v-btn>
+        </v-form>
       </div>
 </template>
 
@@ -15,10 +72,57 @@ export default {
             default: null
         }
     },
-  data(){
-    return {
-      search: '',
+    employeeData: null,
+    data() {
+        return {
+            valid: true,
+            personData: {
+                first_Name: '',
+                last_Name: '',
+                identifier: '',
+                isDeleted: '',
+                email: '',
+                departments: []
+            },
+            nameRules: [
+                v => !!v || 'Field is required',
+                v => (v && v.length <= 10) || 'Must be less than 10 characters',
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            select: null,
+            checkbox: false,
+        }
+    },
+    beforeMount() {
+        const personData = this.$store.state.employees.find(emplo => emplo.identifier === this.employeeId)
+        const personDepartments = personData.employeeDepartments.map(e => ({...e, description: e.employeeDepartment.description}))
+        this.personData = Object.assign({}, personData, { departments: personDepartments })
+    },
+    methods: {
+        updatePersonDetails() {
+            if (this.$refs.form.validate()) {
+                EmployeesService.updateEmployeeDetails(details)
+            } else {
+                // not valid
+            }
+
+        },
+    },
+    computed: {
+        employeeDepartments() {
+            return this.$store.state.departments.map(e => ({  }))
+        }
     }
-  },
 }
 </script>
+
+<style lang="scss" scoped>
+    .edit-employee-container {
+        padding-top: 15px;
+    }
+</style>
+
